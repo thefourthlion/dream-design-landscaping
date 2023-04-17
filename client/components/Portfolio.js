8fb3d8722a8a5da0e94efdb9a80be5459be89981
TheFourthLion <the00110100lion@gmail.com>
2023-04-16 19:22:29 -0700

    create portfolio component

--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Image from "next/image";
+import Img from "../public/images/chainsaw1429x800.webp";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 const Portfolio = () => {
   return (
     <div className="Portfolio">
       <div className="container">
-        <h1 className="content-header">Portfolio</h1>
+        <Row>
+          <Col>
+            <div className="image">
+              <Image
+                priority
+                src={Img}
+                alt="DDL-portfolio-img"
+                width={714}
+                height={400}
+              />
+            </div>
+          </Col>
+
+          <Col>
+            <div className="information">
+              <h1>
+                Experience You Can Count On: A Collection of Our Past Projects
+              </h1>
+              <p>
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt. Neque porro quisquam est, qui
+                dolorem.
+              </p>
+            </div>
+          </Col>
+        </Row>
       </div>
     </div>
   );
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,12 @@
 import Landing from "../components/Landing";
 import Contact from "../components/Contact";
 import Pricing from "../components/Pricing";
+import Portfolio from "../components/Portfolio";
 export default function Home() {
   return (
     <div className="Home">
       <Landing />
+      <Portfolio />
       <Pricing />
       <Contact />
     </div>
