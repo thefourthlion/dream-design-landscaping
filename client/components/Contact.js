eee2f8de1f183549c9da252e7ff189e31b0d5d76
TheFourthLion <the00110100lion@gmail.com>
2023-04-16 18:42:58 -0700

    take floating label out of contact

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -2,7 +2,6 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
 const Contact = () => {
   return (
     <div className="Contact">
