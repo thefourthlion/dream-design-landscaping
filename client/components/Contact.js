6fb09459c45dbed3dca4af80f493a7309418aee4
TheFourthLion <the00110100lion@gmail.com>
2023-04-16 18:41:36 -0700

    create contact component

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 const Contact = () => {
   return (
     <div className="Contact">
       <div className="container">
-        <h1 className="content-header">Contact</h1>
+        <Container>
+          <Row>
+            <Col>
+              <h4>
+                Get in Touch: We're here to help with all your landscaping and
+                tree work needs! If you have any questions or want to request a
+                quote, simply give us a call or send us an email.
+              </h4>
+              <button className="primary-btn">Get Estimate</button>
+              <button className="primary-btn">Contact Us</button>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,10 @@
 import Landing from "../components/Landing";
+import Contact from "../components/Contact";
 export default function Home() {
   return (
     <div className="Home">
       <Landing />
+      <Contact />
     </div>
   );
 }
