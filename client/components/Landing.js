95874cab67225f3cb087f730af3ef181e3599c73
TheFourthLion <the00110100lion@gmail.com>
2023-04-23 09:51:52 -0700

    Add paralaxing effect and carousel

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,16 +1,20 @@
 // import cityVideo from "../public/images/forest.mp4";
+import Img from "../public/images/bark.jpg";
+
 import ReactPlayer from "react-player";
 const Landing = () => {
   return (
     <div className="Landing">
-      {/* <ReactPlayer url={cityVideo} /> */}
-      {/* <video src={cityVideo} type="video/mp4" autoPlay loop muted /> */}
-      <div className="landing-info">
-        <h1>Beautiful landscapes, expertly designed</h1>
+      <div className="img">
+        {/* <ReactPlayer url={cityVideo} /> */}
+        {/* <video src={cityVideo} type="video/mp4" autoPlay loop muted /> */}
+        <div className="landing-info">
+          <h1>Beautiful landscapes, expertly designed</h1>
 
-        <a href="tel:+4733378901">
-          <button className="primary-btn">Get A Quote</button>
-        </a>
+          <a href="tel:+4733378901">
+            <button className="primary-btn">Get A Quote</button>
+          </a>
+        </div>
       </div>
     </div>
   );
--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Image from "next/image";
+import { Carousel } from "react-bootstrap";
+import slideOneImage from "../public/images/chainsaw1429x800.webp";
+import slideTwoImage from "../public/images/chainsaw1429x800.webp";
+import slideThreeImage from "../public/images/chainsaw1429x800.webp";
 import Img from "../public/images/chainsaw1429x800.webp";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -7,27 +11,53 @@ import Container from "react-bootstrap/Container";
 const Portfolio = () => {
   return (
     <div className="Portfolio">
-      <div className="container">
-        <div className="image">
-          <Image
-            className="portfolio-img"
-            priority
-            src={Img}
-            alt="DDL-portfolio-img"
-            width={714}
-            height={400}
-          />
-        </div>
+      <div className="img">
+        <div className="container">
+          <div className="carousel-container">
+            <Carousel className="carousel">
+              <Carousel.Item>
+                <Image
+                  className="carousel-img"
+                  src={slideOneImage}
+                  alt="Slide One"
+                  layout="fill"
+                  objectFit="cover"
+                  priority={true}
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <Image
+                  className="carousel-img"
+                  src={slideTwoImage}
+                  alt="Slide Two"
+                  layout="fill"
+                  objectFit="cover"
+                  priority={true}
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <Image
+                  className="carousel-img"
+                  src={slideThreeImage}
+                  alt="Slide Three"
+                  layout="fill"
+                  objectFit="cover"
+                  priority={true}
+                />
+              </Carousel.Item>
+            </Carousel>
+          </div>
 
-        <div className="information">
-          <h1>
-            Experience You Can Count On: A Collection of Our Past Projects
-          </h1>
-          <p>
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-            fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
-          </p>
+          <div className="information">
+            <h1>
+              Experience You Can Count On: A Collection of Our Past Projects
+            </h1>
+            <p>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit, sed quia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
+            </p>
+          </div>
         </div>
       </div>
     </div>
